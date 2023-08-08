99370d14772d95b562c7d064eb148ac0a0da5890
Mauricio <mauricio.lombano@gmail.com>
2023-08-08 16:51:19 -0600

    Changes to landing

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,19 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        Some good text
-        <div><h1>HTML</h1></div>
+    <main >
+      <div className={styles.base}>
+        <div className={styles.baseNav}>
+          <div className={styles.child}>Alguna descripcion de empresa</div>
+          <div className={styles.child}>Two</div>
+          <div className={styles.child}>Three</div>
+        </div>
+        <div className={styles.baseComponent}>
+          <div className={styles.child}>Four</div>
+          <div className={styles.child}>Five</div>
+          <div className={styles.child}>Six</div>
+        </div>
+      </div>
     </main>
   )
 }
