7e453f35f31128b3338f9d5c268ca1d1de6f0359
Mauricio <mauricio.lombano@gmail.com>
2023-11-07 12:45:02 -0600

    Changes to login and logout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Grid, GridItem, Image, Stack } from '@chakra-ui/react'
+import { Button, Grid, GridItem, Image, Stack, Box, FormControl, FormLabel, Input } from '@chakra-ui/react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import DrawerNavigation from '../components/drawer-menu/nav'
 import { SettingsIcon } from "@chakra-ui/icons"
@@ -16,7 +16,7 @@ import UsersLayout from '../components/users/layout'
 import ResumeBoard from '../components/resume/board'
 import { getUser } from '../lib/requests'
 import { useRouter } from 'next/navigation'
-import Login from '../login/page'
+import { setCookie } from '../lib/manageUserSession'
 
 interface Iuser {
     id: string,
@@ -24,18 +24,57 @@ interface Iuser {
     matriz: string,
     role: string
 }
+const URL = process.env.NEXT_PUBLIC_API_URL
+
 
 export default function Dashboard() {
-    const { data: session } = useSession()
     const router = useRouter()
 
-    const [contentToRender, setContentToRender] = useState(<ResumeBoard />)
+    const [contentToRender, setContentToRender] = useState<JSX.Element | null>(null); // Initialize as null or the default component
+
     const [user, setUser] = useState<Iuser>({ id: "", name: "", matriz: "", role: "" })
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (event: React.FormEvent) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`${URL}login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+
+                // Update user state with retrieved data
+                setUser({
+                    ...user,
+                    id: data.id,
+                    role: data.role,
+                });
+
+                setCookie("userId", data.id, 1);
+                setCookie("userRole", data.role, 1);
+                router.push('/dashboard', { scroll: false });
+            } else {
+                console.error('Login failed');
+            }
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    };
+
+
 
     function desconexion() {
         deleteCookie("userId")
         deleteCookie("userRole")
-        router.push('/login', { scroll: false })
+        setUser({ id: "", name: "", matriz: "", role: "" })
+        router.push('/dashboard', { scroll: false })
 
     }
     const menuOptions = [
@@ -49,37 +88,25 @@ export default function Dashboard() {
         { label: "Desconexion", component: <Button onClick={() => desconexion()}>Cerrar sesion</Button>, data: "" }
     ]
     const menuClientOptions = [
-        { label: "Resumen", component: <ResumeBoard />, data: "" },
+        // { label: "Resumen", component: <ResumeBoard />, data: "" },
         { label: "Cotizar", component: <QuotesBoard />, data: "" },
-        { label: "Generar guias", component: <LabelsBoard />, data: "" },
-        { label: "Guias generadas", component: <LabelsTable />, data: "" },
+        // { label: "Generar guias", component: <LabelsBoard />, data: "" },
+        // { label: "Guias generadas", component: <LabelsTable />, data: "" },
         { label: "Desconexion", component: <Button onClick={() => desconexion()}>Cerrar sesion</Button>, data: "" }
     ]
 
-    async function loadUserFromId(id: string) {
-        const data = await getUser(id)
-        const userParsed = {
-            id: data._id,
-            name: data.userName,
-            matriz: data.provider_access,
-            role: data.role
-        }
-        setUser(userParsed)
-    }
-
 
     useEffect(() => {
-        const data = { userId: getCookie("userId"), userRole: getCookie("userRole") }
-
-        if (data.userId && data.userRole) {
-            loadUserFromId(data.userId)
-
+        if (user.id !== '') {
+            setContentToRender(user.role === 'admin' ? <ResumeBoard /> : <QuotesBoard />);
         }
+    }, [user]);
 
-    }, [])
+
+    let contentToDisplay;
 
     if (user.id !== "") {
-        return (
+        contentToDisplay = (
             <Grid templateColumns='repeat(2, 1fr)' gap={2}>
                 <GridItem>
                     <Button colorScheme='cyan'>
@@ -87,27 +114,57 @@ export default function Dashboard() {
                     </Button>
                     <DrawerNavigation menuOptions={user.role === 'admin' ? menuOptions : menuClientOptions} changeContent={setContentToRender} />
                 </GridItem>
-
                 <GridItem colSpan={4}>
                     {contentToRender}
                 </GridItem>
             </Grid>
-        )
+        );
+    } else {
+        contentToDisplay = (
+            <Grid templateColumns='repeat(2, 1fr)' height="100%">
+                <GridItem height="100%">
+                    <Image minH="40rem" src="https://mumbaimirror.indiatimes.com/photo/81132317.cms" alt="landing-img" />
+                </GridItem>
+                <GridItem padding="20">
+                    <Stack>
+                        <Image src="https://i.ibb.co/0GvqQcH/1-c2-1.jpg" alt="1-c2-1" />
+                        <Box>
+                            <form onSubmit={handleLogin}>
+                                <Grid>
+                                    <GridItem>
+                                        <FormControl>
+                                            <FormLabel>
+                                                Nombre de usuario
+                                            </FormLabel>
+                                            <Input type="text"
+                                                value={username}
+                                                onChange={(e) => setUsername(e.target.value)}
+                                                required />
+                                        </FormControl>
+                                    </GridItem>
+                                    <GridItem>
+                                        <FormControl>
+                                            Contraseña
+                                        </FormControl>
+                                        <Input type="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required />
+                                    </GridItem>
+                                    <GridItem>
+                                        <Button colorScheme='teal' type="submit">
+                                            Conectar
+                                        </Button>
+                                    </GridItem>
+                                </Grid>
+
+                            </form>
+                        </Box>
+                    </Stack>
+                </GridItem>
+            </Grid>
+        );
     }
-    return (
-        <Grid templateColumns='repeat(2, 1fr)' height="100%">
-            <GridItem height="100%">
-                <Image minH="40rem" src="https://mumbaimirror.indiatimes.com/photo/81132317.cms" alt="landing-img" />
-            </GridItem>
-            <GridItem padding="20">
-                <Stack>
-                    <Image src="https://i.ibb.co/0GvqQcH/1-c2-1.jpg" alt="1-c2-1" />
-                    <Login />
-                </Stack>
-            </GridItem>
-            <GridItem bgColor="teal" colSpan={2} >
-                Informacion de pie de pagina
-            </GridItem>
-        </Grid>
-    )
+
+    return contentToDisplay
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,71 +8,12 @@ import { FormControl, FormLabel, Input, Grid, GridItem, Button, Box} from "@chak
 const URL = process.env.NEXT_PUBLIC_API_URL
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const router = useRouter()
-
-    const handleLogin = async (event: React.FormEvent) => {
-        event.preventDefault();
-
-        try {
-            // Your login logic here
-            const response = await fetch(`${URL}login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
-
-            if (response.ok) {
-
-                const data = await response.json()
-                setCookie("userId", data.id, 1)
-                setCookie("userRole", data.role, 1)
-                router.push('/dashboard', { scroll: false })
-            } else {
-                // Handle login failure
-                console.error('Login failed');
-            }
-        } catch (error) {
-            console.error('Login error:', error);
-        }
-    };
+   
 
     return (
-        <Box>
-            <form onSubmit={handleLogin}>
-                <Grid>
-                    <GridItem>
-                        <FormControl>
-                            <FormLabel>
-                                Nombre de usuario
-                            </FormLabel>
-                            <Input type="text"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required />
-                        </FormControl>
-                    </GridItem>
-                    <GridItem>
-                        <FormControl>
-                            Contraseña
-                        </FormControl>
-                        <Input type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
-                    </GridItem>
-                    <GridItem>
-                        <Button colorScheme='teal' type="submit">
-                            Conectar
-                        </Button>
-                    </GridItem>
-                </Grid>
-          
-            </form>
-        </Box>
+      <div>
+        Login 
+      </div>
     );
 };
 
