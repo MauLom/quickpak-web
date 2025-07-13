687dee0ff41933f65233b9d84cf4f8b733618a03
Mauricio Lombano <mauricio.lombano@gmail.com>
2025-07-12 20:32:09 -0600

    Ajustada pagina principal como dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Dashboard from './dashboard/page'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        Test
-    </main>
+    // <main className={styles.main}>
+    //     Test
+    // </main>
+    <Dashboard />
   )
 }
